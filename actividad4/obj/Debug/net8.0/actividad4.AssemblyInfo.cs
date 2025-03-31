d5c57802358e9e5eee43ccf711f352a61f96a870
Mauricio2005 <carlosmauriciochalemendez@gmail.com>
2025-03-31 11:20:36 -0600

    git add.

--- actividad4/obj/Debug/net8.0/actividad4.AssemblyInfo.cs
+++ actividad4/obj/Debug/net8.0/actividad4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("actividad4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5958b1e6de7f7c105046e83257a4e423f5ab25")]
 [assembly: System.Reflection.AssemblyProductAttribute("actividad4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("actividad4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ actividad_arreglos2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = new int[10];
+        int suma = 0;
+
+        Console.WriteLine("Ingrese 10 números enteros:");
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+            suma += numeros[i];
+        }
+
+        double promedio = (double)suma / numeros.Length;
+
+        Console.WriteLine($"\nLa suma total de los valores es: {suma}");
+        Console.WriteLine($"El promedio de los números ingresados es: {promedio:F2}");
+    }
+}
